404393a47c41ff5445efbf84d9452e5d88d53238
Kevin Picard <kevin.picard.au@gmail.com>
2023-03-02 18:06:44 +1100

    First Component (TestChild)

---
+++ src/app/components/TestChild/TestChild.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1 className="text-base">This is my component!</h1>
+      <h1 className="text-base line-through">I'm using TailwindCSS</h1>
+    </div>
+  );
+};
+
+export default MyComponent;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import TestChild from "./components/TestChild/TestChild";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,40 +9,17 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+          <TestChild />
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+          <code className={styles.code}>By{' Kevin Picard '}</code>
+          <Image
+            src="/vercel.svg"
+            alt="Vercel Logo"
+            className={styles.vercelLogo}
+            width={100}
+            height={24}
+            priority
+          />
         </div>
       </div>
 
