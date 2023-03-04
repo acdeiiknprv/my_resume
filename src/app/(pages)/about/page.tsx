263d8e751ea930c0f357e66a1000de55a29d2d57
Kevin Picard <kevin.picard.au@gmail.com>
2023-03-04 21:53:25 +1100

    fix import path

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,7 +1,7 @@
 import TestChild from '@/app/components/TestChild/TestChild';
 import Footer from '@/app/components/Footer/Footer';
 import Menu from '@/app/components/Menu/Menu';
-import styles from '../../page.module.css'
+import styles from '@/app/page.module.css'
 
 export default function About() {
     return (
