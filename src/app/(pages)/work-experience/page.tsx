629a8cfe62ea691afc44a22651943b59fa3ff101
Kevin Picard <kevin.picard.au@gmail.com>
2023-03-04 21:53:15 +1100

    Add routing for all pages

--- src/app/(pages)/work-experience/page.tsx
+++ src/app/(pages)/work-experience/page.tsx
@@ -1,3 +1,16 @@
+import TestChild from '@/app/components/TestChild/TestChild';
+import Footer from '@/app/components/Footer/Footer';
+import Menu from '@/app/components/Menu/Menu';
+import styles from '@/app/page.module.css'
+
 export default function WorkExperience() {
-    return <div>Work Experience</div>
-  }
+    return (
+        <main className={styles.main}>
+            <Menu activeLink="/about" />
+            <div className={styles.description}>
+                <TestChild />
+            </div>
+            <Footer />
+        </main>
+    )
+};
