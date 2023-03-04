5c478f762d9e56acd6a50082d7aa893ee912be12
Kevin Picard <kevin.picard.au@gmail.com>
2023-03-04 21:43:54 +1100

    Adding Active link in Menu + auto

--- src/app/components/Menu/Menu.tsx
+++ src/app/components/Menu/Menu.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import Image from 'next/image'
 import styles from '../../page.module.css'
+import Link from 'next/link'
 
-const Menu = () => {
+interface MenuProps {
+    activeLink: string;
+}
+
+const Menu = (props: MenuProps) => {
+
+    let activeLink = [
+        { name: "Passions", link: "/passions" },
+        { name: "Work Experience", link: "/work-experience" },
+        { name: "About", link: "/about" }
+    ];
+
     return (
         <div className='flex flex-row justify-between w-9/12'>
             <div id='left' className='basis-1/4'>
@@ -18,10 +30,11 @@ const Menu = () => {
 
             <div id='right' className='basis-1/3'>
                 <div id='menu' className='flex flex-row justify-between'>
-                    <p>About</p>
-                    <p>Passions</p>
-                    <p>Portfolio</p>
-                    <p>Contact Me</p>
+                    {activeLink.map((element) => (
+                        <div className={element.link === props.activeLink ? 'activeLink' : ''}>
+                            <Link href={element.link}><p>{element.name}</p></Link>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
