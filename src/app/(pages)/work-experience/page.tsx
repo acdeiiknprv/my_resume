0d93b90be1c95687fcb1dee2a44bed13d1763f31
Kevin Picard <kevin.picard.au@gmail.com>
2023-03-06 08:32:05 +1100

    Add WorkExperience COmponent to Architecture

--- src/app/(pages)/work-experience/page.tsx
+++ src/app/(pages)/work-experience/page.tsx
@@ -3,13 +3,14 @@ import Footer from '@/app/components/Footer/Footer';
 import Menu from '@/app/components/Menu/Menu';
 import styles from '@/app/page.module.css'
 import FilterSearch from '@/app/components/FilterSearch/FilterSearch';
+import WorkExperience from '@/app/components/WorkExperience/WorkExperience';
 
-export default function WorkExperience() {
+export default function WorkExp() {
     return (
         <main className={styles.main}>
             <Menu activeLink="/about" />
             <div className={styles.description}>
-                <FilterSearch />
+                <WorkExperience toActivate={{ keywords: [], country: [], jobTitle: []}} />
             </div>
             <Footer />
         </main>
