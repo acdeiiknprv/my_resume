3514189d172b2a256c76616a57bdf818ed908136
Kevin Picard <kevin.picard.au@gmail.com>
2023-03-05 12:25:56 +1100

    Add FilterSearch + useclient on ReactComponent

--- src/app/(pages)/work-experience/page.tsx
+++ src/app/(pages)/work-experience/page.tsx
@@ -2,13 +2,14 @@ import TestChild from '@/app/components/TestChild/TestChild';
 import Footer from '@/app/components/Footer/Footer';
 import Menu from '@/app/components/Menu/Menu';
 import styles from '@/app/page.module.css'
+import FilterSearch from '@/app/components/FilterSearch/FilterSearch';
 
 export default function WorkExperience() {
     return (
         <main className={styles.main}>
             <Menu activeLink="/about" />
             <div className={styles.description}>
-                <TestChild />
+                <FilterSearch />
             </div>
             <Footer />
         </main>
