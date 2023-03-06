98c76aed974a065fb94bf663acfde47240dd5fce
Kevin Picard <kevin.picard.au@gmail.com>
2023-03-06 16:59:19 +1100

    replace a tag by link

--- src/app/components/ClickableCard/ClickableCard.tsx
+++ src/app/components/ClickableCard/ClickableCard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Image from 'next/image'
 import styles from './ClickableCard.module.css'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,19 +15,14 @@ interface ClickableCardProps {
 const ClickableCard: React.FC<ClickableCardProps> = (props) => {
     return (
         <div className='flex flex-row justify-between w-9/12 text-center' >
-            <a
-                href={props.href}
-                className={styles.card}
-                target="_blank"
-                rel="noopener noreferrer"
-            >
+            <Link href={props.href}>
                 <h2 className={inter.className}>
                     {props.title} <span>-&gt;</span>
                 </h2>
                 <p className={inter.className}>
                     {props.description}
                 </p>
-            </a>
+            </Link>
         </div>
     );
 };
