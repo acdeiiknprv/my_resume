fecf5f2e825dc118ff5b2166e6d939c4316926dd
Kevin Picard <kevin.picard.au@gmail.com>
2023-03-06 21:15:12 +1100

    import style

--- src/app/components/Menu/Menu.tsx
+++ src/app/components/Menu/Menu.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Image from 'next/image'
 import styles from '../../page.module.css'
 import Link from 'next/link'
+import menuStyle from './Menu.module.css'
 
 interface MenuProps {
     activeLink: string;
@@ -16,28 +17,30 @@ const Menu = (props: MenuProps) => {
     ];
 
     return (
-        <div className='flex flex-row justify-between w-9/12 menu'>
-            <div id='left' className='basis-1/4'>
-                {/*TODO: Make div size of image*/}
-                <Link href='/'>
-                    <Image
-                        src="/vercel.svg"
-                        alt="Vercel Logo"
-                        className={styles.vercelLogo}
-                        width={100}
-                        height={24}
-                        priority
-                    />
-                </Link>
-            </div>
+        <div className={menuStyle.menu}>
+            <div className='flex flex-row justify-between w-9/12'>
+                <div id='left' className='basis-1/4'>
+                    {/*TODO: Make div size of image*/}
+                    <Link href='/'>
+                        <Image
+                            src="/vercel.svg"
+                            alt="Vercel Logo"
+                            className={styles.vercelLogo}
+                            width={100}
+                            height={24}
+                            priority
+                        />
+                    </Link>
+                </div>
 
-            <div id='right' className='basis-1/3'>
-                <div id='menu' className='flex flex-row justify-between'>
-                    {links.map((element) => (
-                        <div key={element.name} className={element.link === props.activeLink ? 'activeLink' : ''}>
-                            <Link href={element.link}><p>{element.name}</p></Link>
-                        </div>
-                    ))}
+                <div id='right' className='basis-1/3'>
+                    <div id='menu' className='flex flex-row justify-between'>
+                        {links.map((element) => (
+                            <div key={element.name} className={element.link === props.activeLink ? 'activeLink' : ''}>
+                                <Link href={element.link}><p>{element.name}</p></Link>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
