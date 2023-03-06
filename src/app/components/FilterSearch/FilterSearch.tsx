be2f0af0ba8bc7877fc8d0fa4ec89ce99b781ccb
Kevin Picard <kevin.picard.au@gmail.com>
2023-03-06 19:01:43 +1100

    click not trigger ?

--- src/app/components/FilterSearch/FilterSearch.tsx
+++ src/app/components/FilterSearch/FilterSearch.tsx
@@ -140,62 +140,60 @@ const FilterSearch: React.FC<Props> = ({
     }
     return (
         <div>
-            <aside id="sidebar-multi-level-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-                    <ul className="space-y-2">
-                        <li>
-                            <button type="button" className="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
-                                <span className="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item>Tech</span>
-                                <svg sidebar-toggle-item className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                            </button>
-                            <ul id="dropdown-example" className="hidden py-2 space-y-2">
+            <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+                <ul className="space-y-2">
+                    <li>
+                        <button type="button" className="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                            <span className="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item>Tech</span>
+                            <svg sidebar-toggle-item className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        </button>
+                        <ul id="dropdown-example" className="hidden py-2 space-y-2">
 
-                                {filters?.keywords.map((keyword: FilterField) => {
-                                    return (
-                                        <li key={keyword.name}>
-                                            <a onClick={() => activateFilter("keywords", keyword.name)} href="#" className="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">{keyword.name}</a>
-                                        </li>
-                                    );
-                                })}
-                            </ul>
-                        </li>
+                            {filters?.keywords.map((keyword: FilterField) => {
+                                return (
+                                    <li key={keyword.name}>
+                                        <a onClick={() => activateFilter("keywords", keyword.name)} href="#" className="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">{keyword.name}</a>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </li>
 
-                        <li>
-                            <button type="button" className="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
-                                <span className="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item>Country</span>
-                                <svg sidebar-toggle-item className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                            </button>
-                            <ul id="dropdown-example" className="hidden py-2 space-y-2">
+                    <li>
+                        <button type="button" className="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                            <span className="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item>Country</span>
+                            <svg sidebar-toggle-item className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        </button>
+                        <ul id="dropdown-example" className="hidden py-2 space-y-2">
 
-                                {filters?.country.map((country: FilterField) => {
-                                    return (
-                                        <li key={country.name}>
-                                            <a onClick={() => activateFilter("country", country.name)} href="#" className="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">{country.name}</a>
-                                        </li>
-                                    );
-                                })}
-                            </ul>
-                        </li>
+                            {filters?.country.map((country: FilterField) => {
+                                return (
+                                    <li key={country.name}>
+                                        <a onClick={() => activateFilter("country", country.name)} href="#" className="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">{country.name}</a>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </li>
 
-                        <li>
-                            <button type="button" className="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
-                                <span className="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item>Job Title</span>
-                                <svg sidebar-toggle-item className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                            </button>
-                            <ul id="dropdown-example" className="hidden py-2 space-y-2">
+                    <li>
+                        <button type="button" className="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                            <span className="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item>Job Title</span>
+                            <svg sidebar-toggle-item className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        </button>
+                        <ul id="dropdown-example" className="hidden py-2 space-y-2">
 
-                                {filters?.jobTitle.map((jobTitle: FilterField) => {
-                                    return (
-                                        <li key={jobTitle.name}>
-                                            <a onClick={() => activateFilter("jobTitle", jobTitle.name)} href="#" className="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">{jobTitle.name}</a>
-                                        </li>
-                                    );
-                                })}
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </aside>
+                            {filters?.jobTitle.map((jobTitle: FilterField) => {
+                                return (
+                                    <li key={jobTitle.name}>
+                                        <a onClick={() => activateFilter("jobTitle", jobTitle.name)} href="#" className="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">{jobTitle.name}</a>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 };
