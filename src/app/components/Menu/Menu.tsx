a0ebc69696fd8a1f9d95e2ace4f7858f6b30f61d
Kevin Picard <kevin.picard.au@gmail.com>
2023-03-06 21:30:35 +1100

    SetBack menu

--- src/app/components/Menu/Menu.tsx
+++ src/app/components/Menu/Menu.tsx
@@ -14,30 +14,28 @@ const Menu = (props: MenuProps) => {
     ];
 
     return (
-        <div className={menuStyle.menu}>
-            <div className='flex flex-row justify-between w-9/12'>
-                <div id='left' className='basis-1/4'>
-                    {/*TODO: Make div size of image*/}
-                    <Link href='/'>
-                        <Image
-                            src="/vercel.svg"
-                            alt="Vercel Logo"
-                            className={styles.vercelLogo}
-                            width={100}
-                            height={24}
-                            priority
-                        />
-                    </Link>
-                </div>
+        <div className='flex flex-row justify-between w-9/12'>
+            <div id='left' className='basis-1/4'>
+                {/*TODO: Make div size of image*/}
+                <Link href='/'>
+                    <Image
+                        src="/vercel.svg"
+                        alt="Vercel Logo"
+                        className={styles.vercelLogo}
+                        width={100}
+                        height={24}
+                        priority
+                    />
+                </Link>
+            </div>
 
-                <div id='right' className='basis-1/3'>
-                    <div id='menu' className='flex flex-row justify-between'>
-                        {links.map((element) => (
-                            <div key={element.name} className={element.link === props.activeLink ? 'activeLink' : ''}>
-                                <Link href={element.link}><p>{element.name}</p></Link>
-                            </div>
-                        ))}
-                    </div>
+            <div id='right' className='basis-1/3'>
+                <div id='menu' className='flex flex-row justify-between'>
+                    {links.map((element) => (
+                        <div key={element.name} className={element.link === props.activeLink ? 'activeLink' : ''}>
+                            <Link href={element.link}><p>{element.name}</p></Link>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
