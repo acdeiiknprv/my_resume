2393aa1b3c4cf1348520ca2e44a971cbe43bca70
Kevin Picard <kevin.picard.au@gmail.com>
2023-03-06 16:59:08 +1100

    update Work page to handle predefined filters

--- src/app/(pages)/work-experience/page.tsx
+++ src/app/(pages)/work-experience/page.tsx
@@ -4,13 +4,52 @@ import Menu from '@/app/components/Menu/Menu';
 import styles from '@/app/page.module.css'
 import FilterSearch from '@/app/components/FilterSearch/FilterSearch';
 import WorkExperience from '@/app/components/WorkExperience/WorkExperience';
+import { useRouter } from 'next/router';
+
+interface FilterField {
+    name: string;
+    active: boolean;
+}
+
+interface filter {
+    keywords: Array<FilterField>,
+    country: Array<FilterField>,
+    jobTitle: Array<FilterField>
+};
 
 export default function WorkExp() {
+    const router = useRouter();
+    const { filter } = router.query;
+
+    let filtersToActivate: filter = {
+        keywords: [],
+        country: [],
+        jobTitle: []
+      };
+
+    if (filter) {
+        switch (filter) {
+            case 'front-end':
+              filtersToActivate.jobTitle.push({ name: "FullStack Developer", active: true });
+              filtersToActivate.jobTitle.push({ name: "Web Developer", active: true });
+              break;
+            case 'back-end':
+              filtersToActivate.jobTitle.push({ name: "FullStack Developer", active: true });
+              filtersToActivate.jobTitle.push({ name: "DevOps Engineer", active: true });
+              break;
+            case 'mobile':
+              filtersToActivate.jobTitle.push({ name: "Android Developer", active: true });
+              break;
+            default:
+              break;
+          }
+    }
+
     return (
         <main className={styles.main}>
             <Menu activeLink="/about" />
             <div className={styles.description}>
-                <WorkExperience toActivate={{ keywords: [], country: [], jobTitle: []}} />
+                <WorkExperience toActivate={filtersToActivate} />
             </div>
             <Footer />
         </main>
