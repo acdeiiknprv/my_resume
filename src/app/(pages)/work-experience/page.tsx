b98c30091631d3c48e33752ed39bd016eaba5e74
Kevin Picard <kevin.picard.au@gmail.com>
2023-03-06 17:20:35 +1100

    useRouter ?

--- src/app/(pages)/work-experience/page.tsx
+++ src/app/(pages)/work-experience/page.tsx
@@ -1,10 +1,8 @@
-'use client'
 import Footer from '@/app/components/Footer/Footer';
 import Menu from '@/app/components/Menu/Menu';
 import styles from '@/app/page.module.css'
 import WorkExperience from '@/app/components/WorkExperience/WorkExperience';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 
 interface FilterField {
     name: string;
@@ -17,37 +15,35 @@ interface filter {
     jobTitle: Array<FilterField>
 };
 
+
 export default function WorkExp() {
 
-
     const router = useRouter();
+    const filter = router.query.filter || '';
 
     let filtersToActivate: filter = {
         keywords: [],
         country: [],
         jobTitle: []
     };
-    
-    useEffect(() => {
-        const filter = router.query.filter;
-        if (filter) {
-            switch (filter) {
-                case 'front-end':
-                    filtersToActivate.jobTitle.push({ name: "FullStack Developer", active: true });
-                    filtersToActivate.jobTitle.push({ name: "Web Developer", active: true });
-                    break;
-                case 'back-end':
-                    filtersToActivate.jobTitle.push({ name: "FullStack Developer", active: true });
-                    filtersToActivate.jobTitle.push({ name: "DevOps Engineer", active: true });
-                    break;
-                case 'mobile':
-                    filtersToActivate.jobTitle.push({ name: "Android Developer", active: true });
-                    break;
-                default:
-                    break;
-            }
+
+    if (filter) {
+        switch (filter) {
+            case 'front-end':
+                filtersToActivate.jobTitle.push({ name: "FullStack Developer", active: true });
+                filtersToActivate.jobTitle.push({ name: "Web Developer", active: true });
+                break;
+            case 'back-end':
+                filtersToActivate.jobTitle.push({ name: "FullStack Developer", active: true });
+                filtersToActivate.jobTitle.push({ name: "DevOps Engineer", active: true });
+                break;
+            case 'mobile':
+                filtersToActivate.jobTitle.push({ name: "Android Developer", active: true });
+                break;
+            default:
+                break;
         }
-    }, []);
+    }
 
     return (
         <main className={styles.main}>
