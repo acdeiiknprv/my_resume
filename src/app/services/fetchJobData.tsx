65628d1833c6368fc510462e2d1c123f1a7bc780
Kevin Picard <kevin.picard.au@gmail.com>
2023-09-12 17:11:50 +1000

    update link

--- src/app/services/fetchJobData.tsx
+++ src/app/services/fetchJobData.tsx
@@ -1,5 +1,5 @@
 export async function getJobsData() {
-    const res = await fetch(`https://my-resume-backend.vercel.app/jobs`)
+    const res = await fetch(`https://my-resume-backend-prisma.vercel.app/jobs`)
     const data = await res.json()
     return data
   }
