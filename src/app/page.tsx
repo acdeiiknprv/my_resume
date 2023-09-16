0dd00be1ad63b09dc77f11974ee2995338066ce6
Kevin Picard <kevin.picard.au@gmail.com>
2023-09-16 22:48:19 +1000

    R01: Main Page (WIP)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import TestChild from "./components/TestChild/TestChild";
 import Footer from './components/Footer/Footer';
 import Menu from './components/Menu/Menu';
 import ClickableCard from './components/ClickableCard/ClickableCard';
+import { Box, Typography } from '@mui/material';
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -14,54 +15,79 @@ export default function Home() {
     <main className={styles.main}>
       <Menu activeLink='' />
 
-      <div className='mb-32'>
-        <div className='flex flex-row justify-between mb-32'>
-          <div className="basis-3/5">
-            <span className="text-3xl">Hi !</span>
-            <br />
-            <span className="text-4xl font-bold">I&apos;m Kevin Picard,</span>
-            <br />
-            <span className="text-4xl font-bold">a FullStack Developer</span>
-          </div>
+      <Box width={'75%'}>
+        <Box display={'flex'} justifyContent={'space-around'} alignContent={'center'}>
+          <Box display={'flex'} flexDirection={'column'} justifyContent={'center'}>
+            <Typography variant={'h5'}>
+              Hi !
+            </Typography>
+            <Typography variant={'h4'} fontWeight={'700'}>
+              I&apos;m Kevin Picard,
+            </Typography>
+            <Typography variant={'h4'} fontWeight={'700'}>
+              a FullStack Developer
+            </Typography>
+          </Box>
 
-          <div className="basis-2/5">
+          <Box> {/* Add picture of me */}
             <Image
               src="/main.png"
               alt="Developer picture"
               className=""
-              width={500}
-              height={465}
+              width={250}
+              height={250}
               priority
             />
-          </div>
-        </div>
+          </Box>
+        </Box>
 
-        <div className={styles.grid}>
-          <ClickableCard
-            title="FullStack"
-            query="full-stack"
-            pathname="/work-experience"
-            description="(JavaScript)"
-            picture="/fullstack.png"
-          />
+        <Box margin={'10vh 0px'}>
+          {/* Include desc here */}
+          <Typography variant={'h4'} fontWeight={'700'} textAlign={'center'}>
+            Insert Description (2 lines)
+          </Typography>
+        </Box>
 
-          <ClickableCard
-            title="DevOps"
-            query="dev-ops"
-            pathname="/work-experience"
-            description="(AWS, Docker, Kubernetes)"
-            picture="/devops.png"
-          />
+        <Box> {/* To move to about + replace by Technology used */}
+          <Box textAlign={'center'} marginBottom={'5vh'}>
+            <Typography variant={'h4'} fontWeight={'700'}>
+              ~ My Tech Stack ~
+            </Typography>
+          </Box>
+          <Box display={'flex'} justifyContent={'space-around'}>
+            
+            <ClickableCard
+                title="DevOps"
+                query="dev-ops"
+                pathname="/work-experience"
+                picture="/devops.png"
+              />
+              
+              <ClickableCard
+                title="FullStack"
+                query="full-stack"
+                pathname="/work-experience"
+                picture="/fullstack.png"
+              />
 
-          <ClickableCard
-            title="Mobile"
-            query="mobile"
-            pathname="/work-experience"
-            description="(Kotlin, React Native)"
-            picture="/mobile.png"
-          />
-        </div>
-      </div>
+              <ClickableCard
+                title="Mobile"
+                query="mobile"
+                pathname="/work-experience"
+                picture="/mobile.png"
+              />
+
+          </Box>
+        </Box>
+
+        <Box margin={'10vh 0px 0px 0px'}>
+          {/* Include quote here */}
+          <Typography variant={'h4'} fontWeight={'700'} textAlign={'center'}>
+            Insert quote here
+          </Typography>
+        </Box>
+      </Box>
+
       <Footer />
     </main>
   )
