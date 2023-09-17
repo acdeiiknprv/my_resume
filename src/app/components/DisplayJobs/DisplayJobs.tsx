24b97c0fcabf33f1b9c0e8c1ef15d968a9decf87
Kevin Picard <kevin.picard.au@gmail.com>
2023-09-17 13:36:50 +1000

    R01 - DisplayJobs v1.0

--- src/app/components/DisplayJobs/DisplayJobs.tsx
+++ src/app/components/DisplayJobs/DisplayJobs.tsx
@@ -1,37 +1,58 @@
 import React from 'react';
 import { Inter } from 'next/font/google'
 import { DisplayJobsProps, JobsData } from '@/app/interfaces/interfaces';
+import styles from './DisplayJobs.module.css'
+import { Box, Typography } from '@mui/material';
+import { useThemeContext } from '@/app/styles/ThemeContext';
 
 const inter = Inter({ subsets: ['latin'] })
 
 const DisplayJobs: React.FC<DisplayJobsProps> = (props) => {
+
+    const { theme } = useThemeContext();
+
+    const findBorderColorByJobTitleAndTheme = (jobTitle: string) => {
+        if (theme.palette.mode === 'dark') {
+            if (jobTitle.toLocaleLowerCase().includes('fullstack')) {
+                return styles.fullstackDark;
+            } else if (jobTitle.toLocaleLowerCase().includes('devops')) {
+                return styles.devopsDark;
+            } else if (jobTitle.toLocaleLowerCase().includes('android')) {
+                return styles.mobileDark;
+            }
+        } else if (theme.palette.mode === 'light') {
+            if (jobTitle.toLocaleLowerCase().includes('fullstack')) {
+                return styles.fullstackLight;
+            } else if (jobTitle.toLocaleLowerCase().includes('devops')) {
+                return styles.devopsLight;
+            } else if (jobTitle.toLocaleLowerCase().includes('android')) {
+                return styles.mobileLight;
+            }
+        }
+    }
+
     return (
         <div>
             {props.filteredJobs.map((job: JobsData, index: number) => {
                 return (
-                    <div key={index}>
-                        <div className=''>
-                            <br />
-                            <h2 className="text-xl">
+                    <Box key={index} margin={'2vh'}>
+                        <Box border={'2px solid'} className={findBorderColorByJobTitleAndTheme(job.jobTitle)} borderRadius={'8px'} padding={'3vh'}>
+                            <Typography variant='h5' fontWeight={'700'}>
                                 {job.jobTitle} @ <span className='underline'>{job.jobCompany}</span>
-                            </h2>
-                            <h3 className="text-lg">
+                            </Typography>
+                            <Typography fontWeight={'700'}>
                                 {job.jobEndDate === "now" ? (
                                     job.jobCityLocation + ', ' + job.jobCountryLocation + ' - Since ' + job.jobStartDate
                                 ) : (
                                     job.jobCityLocation + ', ' + job.jobCountryLocation + ' - From ' + job.jobStartDate + ' to ' + job.jobEndDate
                                 )}
-                            </h3>
+                            </Typography>
                             <br />
-                            <span className='text-base'>
-                                {job.jobDescription}
-                            </span>
-                            <br />
+                            <Typography dangerouslySetInnerHTML={{ __html: job.jobDescription }} />
                             <br />
                             <span className='text-sm'>Keywords: {job.jobKeywords.join(' ')}</span>
-                        </div>
-                        <hr />
-                    </div>
+                        </Box>
+                    </Box>
                 )
             })}
         </div>
