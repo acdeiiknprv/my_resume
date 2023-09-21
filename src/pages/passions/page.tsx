21b47f36bec3ac0577f70211a954da739c66f672
Kevin Picard <kevin.picard.au@gmail.com>
2023-09-21 22:38:01 +1000

    clean passion page

--- src/pages/passions/page.tsx
+++ src/pages/passions/page.tsx
@@ -1,16 +1,8 @@
-import TestChild from '@/app/components/TestChild/TestChild';
-import Footer from '@/app/components/Footer/Footer';
-import Menu from '@/app/components/Menu/Menu';
-import styles from '@/app/page.module.css'
-
 export default function Passions() {
     return (
-        <main className={styles.main}>
-            <Menu activeLink="/" />
-            <div className={styles.description}>
-                <TestChild />
+        <main>
+            <div>
             </div>
-            <Footer />
         </main>
     )
 };
