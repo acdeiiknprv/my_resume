659d61faf98bda8a963c749ac9711507a8d55091
Kevin Picard <kevin.picard.au@gmail.com>
2023-09-21 22:37:04 +1000

    clean about page

--- src/pages/about/page.tsx
+++ src/pages/about/page.tsx
@@ -1,4 +1,3 @@
-import TestChild from '@/app/components/TestChild/TestChild';
 import Footer from '@/app/components/Footer/Footer';
 import Menu from '@/app/components/Menu/Menu';
 import styles from '@/app/page.module.css'
@@ -6,11 +5,8 @@ import styles from '@/app/page.module.css'
 export default function About() {
     return (
         <main className={styles.main}>
-            <Menu activeLink="/" />
-            <div className={styles.description}>
-                <TestChild />
+            <div>
             </div>
-            <Footer />
         </main>
     )
 };
